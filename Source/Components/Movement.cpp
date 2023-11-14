2c3cd858da12b0588d4e3809be3bdd920298b1a4
CI <>
2023-11-14 16:36:34 +0900

    MovementComponentをリファクタリング ステータスを登録後、任意のところで加速減速を呼び出す感じで使う

--- Source/Components/Movement.cpp
+++ Source/Components/Movement.cpp
@@ -1,51 +1,66 @@
 #include "Movement.h"
+#include "../../Actor.h"
 
 Movement::Movement(Actor* owner)
 	: Component(owner),
-	direction(0.0f, 0.0f),
-	velocity(0.0f, 0.0f),
-	speed(0.0f)
+	direction_(0.0f, 0.0f),
+	velocity_(0.0f, 0.0f),
+	acceleration_(0.0f),
+	decelerationRate_(),
+	maxSpeed_(0.0f),
+	stopSpeed_(0.0f),
+	initSpeed_(0.0f),
+	nowSpeed_(initSpeed_)
 {
 }
-Movement::Movement(class Actor* owner,float speed_)
+Movement::Movement(class Actor* owner, float initSpeed)
 	: Component(owner),
-	direction(0.0f, 0.0f),
-	velocity(0.0f, 0.0f),
-	speed(speed_)
+	direction_(0.0f, 0.0f),
+	velocity_(0.0f, 0.0f),
+	acceleration_(0.0f),
+	decelerationRate_(),
+	maxSpeed_(0.0f),
+	stopSpeed_(0.0f),
+	initSpeed_(initSpeed),
+	nowSpeed_(initSpeed_)
 {
 }
 
 float Movement::GetSpeed() const
 {
-	return speed;
+	return nowSpeed_;
 }
 ML::Vec2 Movement::GetDirection() const
 {
-	return direction;
+	return direction_;
 }
 ML::Vec2 Movement::GetVelocity() const
 {
-	return velocity;
+	return velocity_;
 }
 
-void Movement::SetSpeed(float speed_)
+void Movement::SetSpeed(const float speed)
 {
-	if (speed == speed_)
+	if (nowSpeed_ == speed)
 		return;
-	speed = speed_;
-	velocity = direction * speed;
+	nowSpeed_ = speed;
+	CalcVelocity();
 }
-void Movement::SetDirection(ML::Vec2 direction_)
+void Movement::SetDirection(const ML::Vec2& direction)
 {
-	if (direction == direction_)
+	if (direction_ == direction)
 		return;
-	direction = direction_;
-	velocity = direction * speed;
+	direction_ = direction;
+	CalcVelocity();
+}
+void Movement::SetDecelerationRate(const ML::Percentage& decelerationRate)
+{
+	decelerationRate_ = decelerationRate;
 }
 
-void Movement::LStickInputToMoveVelocity(XI::GamePad::SP controller_)
+void Movement::LStickInputToMoveVelocity(XI::GamePad::SP controller)
 {
-	auto inp = controller_->GetState();
+	auto inp = controller->GetState();
 	int key = 0;
 	if (inp.LStick.BU.on)
 		key += 1;
@@ -59,29 +74,53 @@ void Movement::LStickInputToMoveVelocity(XI::GamePad::SP controller_)
 	SetMoveFromKey(key);
 }
 
-void Movement::SetMoveFromKey(int key_)
+void Movement::SetMoveFromKey(int key)
 {
 	//0, 1, 2,  3,  4,  5,  6, 7, 8, 9,10,
 	int table[] = { -1,90,270,-1,180,135,225,-1, 0,45,315 };
 
 	ML::Vec2 est;
 	//0‚Í“ÁŽêðŒ
-	if (key_ == 0)
+	if (key == 0)
 	{
 		est.x = 0.f;
 		est.y = 0.f;
 	}
 	else
 	{
-		est.x = (float) cos(table[key_] * D3DX_PI / 180.f);
-		est.y = (float)-sin(table[key_] * D3DX_PI / 180.f);
+		est.x = (float) cos(table[key] * D3DX_PI / 180.f);
+		est.y = (float)-sin(table[key] * D3DX_PI / 180.f);
 	}
 
-	direction = est;
-	velocity = direction * speed;
+	direction_ = est;
+	CalcVelocity();
+}
+ML::Vec2 Movement::CalcVelocity(float speed, ML::Vec2 direction)
+{
+	return direction * speed;
 }
 
-void Movement::MoveOn(ML::Vec2& pos)
+void Movement::CalcVelocity()
 {
-	pos += velocity;
+	velocity_ = CalcVelocity(nowSpeed_, direction_);
+}
+
+void Movement::Accel()
+{
+	nowSpeed_ += acceleration_ / REFRESHRATE;
+	if (nowSpeed_ > maxSpeed_)
+		nowSpeed_ = maxSpeed_;
+
+	SetSpeed(nowSpeed_);
+	owner_->pos_ += velocity_;
+}
+
+void Movement::Decel()
+{
+	nowSpeed_ *= decelerationRate_.Get() / REFRESHRATE;
+	if (nowSpeed_ <= stopSpeed_)
+		nowSpeed_ = 0.0f;
+
+	SetSpeed(nowSpeed_);
+	owner_->pos_ += velocity_;
 }
