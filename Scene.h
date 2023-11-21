a377b5a4316615f3afa1266c16144dbe11e4c878
CI <>
2023-11-21 16:10:52 +0900

    トグルボタン、シーン遷移ボタン、シーンの基底クラス作成、ショップシーン追加

---
+++ Scene.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "Actor.h"
+
+class ToggleButton;
+
+class Scene : public GameObject
+{
+public:
+	shared_ptr<Scene> SP;
+	weak_ptr<Scene> WP;
+
+	enum Kind
+	{
+		Title,
+		Base,
+		Shop,
+		Mining,
+		MartialFight,
+		Ending
+	};
+private:
+	Kind nextScene_;
+	vector<shared_ptr<ToggleButton>> sceneChangeButtons_;
+public:
+	void SetNextScene(const Kind nextScene);
+	void AddSceneChangeButton(const shared_ptr<ToggleButton>& sceneChangeButton);
+	void UpDate() override;
+	void CreateNextScene();
+	Scene();
+	virtual ~Scene();
+};
+
---
+++ Source/Actors/UI/SceneChangeButton.h
@@ -0,0 +1,62 @@
+#pragma warning(disable:4996)
+#pragma once
+//-------------------------------------------------------------------
+//シーンを切り替えるボタン
+//-------------------------------------------------------------------
+#include "../../../ToggleButton.h"
+#include "../../../Scene.h"
+
+namespace SceneChangeButton
+{
+	//タスクに割り当てるグループ名と固有名
+	const  string  defGroupName("UI");					//グループ名
+	const  string  defName("SceneChangeButton");		//タスク名
+	//-------------------------------------------------------------------
+	class  Resource : public BResource
+	{
+		bool  Initialize()		override;
+		bool  Finalize()		override;
+		Resource();
+	public:
+		~Resource();
+		typedef  shared_ptr<Resource>	SP;
+		typedef  weak_ptr<Resource>		WP;
+		static   WP  instance;
+		static  Resource::SP  Create();
+	};
+	//-------------------------------------------------------------------
+	class  Object : public  ToggleButton
+	{
+	public:
+		virtual  ~Object();
+		typedef  shared_ptr<Object>		SP;
+		typedef  weak_ptr<Object>		WP;
+		typedef  shared_ptr<vector<Object::SP>> SPV;
+		//生成窓口 引数はtrueでタスクシステムへ自動登録
+		static  Object::SP  Create(bool flagGameEnginePushBack_);
+		Resource::SP	res;
+
+	private:
+
+		Object();
+		bool  B_Initialize();
+		bool  B_Finalize();
+		bool  Initialize();	//「初期化」タスク生成時に１回だけ行う処理
+		void  UpDate()		override;	//「実行」１フレーム毎に行う処理
+		void  Render2D_AF()	override;	//「2D描画」１フレーム毎に行う処理
+		bool  Finalize();		//「終了」タスク消滅時に１回だけ行う処理
+
+	private:
+
+		Scene* nowScene_;
+		Scene::Kind nextScene_;
+
+	public:
+		void SetScene(Scene* nowScene, const Scene::Kind& nextScene);
+
+		void OnEvent() override;
+		void OffEvent() override;
+
+		string debugText = "NON";
+	};
+}
